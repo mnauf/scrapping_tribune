755fa6f3360245dfd61988a5a5831836298c615a
Naufil <m.naufil1@gmail.com>
2020-05-30 23:35:40 +0500

    csv file added

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
@@ -32,3 +34,12 @@ if __name__ == '__main__':
         print("Title: ",title_list[i])
         print("Description: ",description_list[i])
         print()
+    
+    s1 = pd.Series(title_list[0:14], name='News Heading')
+    s2 = pd.Series(description_list[0:14], name='News Description')
+    df = pd.concat([s1,s2], axis=1)
+    df = df.fillna("None")
+    # d = {'Phone name': phone_name_list, 'Price per month': price_per_month_list,'Interest List': interest_list, 'Total Price List': total_price_list,'Unlimited Offer List': unlimited_offer_list}
+    # df = pd.DataFrame(data=d)
+    print(df)
+    df.to_csv('tribune.csv', encoding='utf-8')
